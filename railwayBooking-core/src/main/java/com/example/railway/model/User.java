88e09274ab4cd82058c416e31a6c3b78bd26c881
LohithCsk <74369740+LohithCsk@users.noreply.github.com>
2023-04-15 10:27:47 -0500

    Added user model

---
+++ railwayBooking-core/src/main/java/com/example/railway/model/User.java
@@ -0,0 +1,88 @@
+package com.example.railway.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserType userType;
+    
+    @Column(nullable=false)
+    private String email;
+    
+    // Other user-related fields (e.g., name, email, etc.)
+    
+    // Getters and Setters
+    
+    public enum UserType {
+        CUSTOMER,
+        ADMIN
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserType getUserType() {
+		return userType;
+	}
+
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", userType=" + userType
+				+ ", email=" + email + "]";
+	}
+    
+}
+
